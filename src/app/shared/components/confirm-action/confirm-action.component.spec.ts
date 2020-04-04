a1b4b84bf644ee9456b64c00560b8cae1a233c23
Hector Arenas <hector.it.dev@gmail.com>
2020-04-04 10:21:48 +0200

    fix mat dialog unit test issues

--- src/app/shared/components/confirm-action/confirm-action.component.spec.ts
+++ src/app/shared/components/confirm-action/confirm-action.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ConfirmActionComponent } from './confirm-action.component';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 describe('ConfirmActionComponent', () => {
   let component: ConfirmActionComponent;
@@ -8,7 +9,11 @@ describe('ConfirmActionComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ConfirmActionComponent ]
+      declarations: [ ConfirmActionComponent ],
+      providers: [
+        {provide: MAT_DIALOG_DATA, useValue: {}},
+        {provide: MatDialogRef, useValue: {}}
+      ]
     })
     .compileComponents();
   }));
