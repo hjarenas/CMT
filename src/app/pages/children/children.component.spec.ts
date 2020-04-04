cca0c2d16332bd57cf5114fc1445b17137e3a3fe
Hector Arenas <hector.it.dev@gmail.com>
2020-04-04 10:34:57 +0200

    fix angular + material unit test issues

--- src/app/pages/children/children.component.spec.ts
+++ src/app/pages/children/children.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChildrenComponent } from './children.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ChildrenComponent', () => {
   let component: ChildrenComponent;
@@ -8,6 +9,7 @@ describe('ChildrenComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ ChildrenComponent ]
     })
     .compileComponents();
